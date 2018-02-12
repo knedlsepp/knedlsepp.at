9f07a416a76fbf3d5fa802f9aa711fed8dac687a
Josef Kemetmueller <josef.kemetmueller@gmail.com>
2018-02-12 18:34:31 +0000

    WIP: Try to make a desktop environment work

--- configuration.nix
+++ configuration.nix
@@ -221,5 +221,50 @@ in
     description = "Josef Knedlmüller";
     initialPassword = "foo";
   };
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+  services.xserver.videoDrivers = [ "nouveau" ];
+  services.xserver.enable = true;
+  services.xserver.autorun = false;
+  hardware.opengl.enable = true;
+  hardware.opengl.extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl vaapiVdpau ];
+  hardware.opengl.driSupport = true;
+  hardware.opengl.driSupport32Bit = true;
+  nixpkgs.config.allowUnfree = true;
+
+  systemd.sockets.terminal-server =
+    { description = "Terminal Server Socket";
+      wantedBy = [ "sockets.target" ];
+      before = [ "multi-user.target" ];
+      socketConfig.Accept = true;
+      socketConfig.ListenStream = 5900;
+    };
+
+  systemd.services."terminal-server@" =
+    { description = "Terminal Server";
+
+      path =
+        [ pkgs.xorg.xorgserver.out pkgs.gawk pkgs.which pkgs.openssl pkgs.xorg.xauth
+          pkgs.nettools pkgs.shadow pkgs.procps pkgs.utillinux pkgs.bash
+        ];
+
+      environment.FD_GEOM = "1024x786x24";
+      environment.FD_XDMCP_IF = "127.0.0.1";
+      #environment.FIND_DISPLAY_OUTPUT = "/tmp/foo"; # to debug the "find display" script
+
+      serviceConfig =
+        { StandardInput = "socket";
+          StandardOutput = "socket";
+          StandardError = "journal";
+          ExecStart = "@${pkgs.x11vnc}/bin/x11vnc x11vnc -inetd -display WAIT:1024x786:cmd=FINDCREATEDISPLAY-Xvfb.xdmcp -unixpw -ssl SAVE";
+          # Don't kill the X server when the user quits the VNC
+          # connection.  FIXME: the X server should run in a
+          # separate systemd session.
+          KillMode = "process";
+        };
+    };
+
+
+
 }
 
