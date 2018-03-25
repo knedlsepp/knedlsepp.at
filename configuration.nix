2f53cbc594ab87fce9e4aff05e8503f55b9788fe
Josef Kemetmueller <josef.kemetmueller@gmail.com>
2018-03-25 23:53:33 +0000

    Introduce stateVersion as safeguard

--- configuration.nix
+++ configuration.nix
@@ -237,5 +237,12 @@ in
     description = "Josef Knedlmüller";
     initialPassword = "foo";
   };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "17.09"; # Did you read the comment?
+
 }
 
