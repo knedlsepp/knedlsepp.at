22c275eeb8288867afdfda19124cffca4e7fcac8
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-09-25 21:36:35 +0200

    Factor out domain-name

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,7 @@
 { config, pkgs, ... }:
-{
+let
+  domain-name = "knedlsepp.at";
+in {
   imports = [ <nixpkgs/nixos/modules/virtualisation/amazon-image.nix> ];
   ec2.hvm = true;
 
@@ -57,8 +59,8 @@
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
-    virtualHosts."knedlsepp.at" = {
-      serverAliases = [ "www.knedlsepp.at" ];
+    virtualHosts."${domain-name}" = {
+      serverAliases = [ "www.${domain-name}" ];
       enableACME = true;
       forceSSL = true;
       root = builtins.fetchGit {
@@ -66,22 +68,22 @@
         rev = "6bb09bcca1bd39344d4e568c70b2ad31fd29f1bf";
       };
     };
-    virtualHosts."xn--qeiaa.knedlsepp.at" = { # ❤❤❤.knedlsepp.at - Punycoded
+    virtualHosts."xn--qeiaa.${domain-name}" = { # ❤❤❤.${domain-name} - Punycoded
       serverAliases = [
-        "xn--c6haa.knedlsepp.at"
-        "xn--yr8haa.knedlsepp.at"
-        "xn--0r8haa.knedlsepp.at"
-        "xn--1r8haa.knedlsepp.at"
-        "xn--2r8haa.knedlsepp.at"
-        "xn--3r8haa.knedlsepp.at"
-        "xn--4r8haa.knedlsepp.at"
-        "xn--5r8haa.knedlsepp.at"
-        "xn--6r8haa.knedlsepp.at"
-        "xn--7r8haa.knedlsepp.at"
-        "xn--8r8haa.knedlsepp.at"
-        "xn--9r8haa.knedlsepp.at"
-        "xn--g6haa.knedlsepp.at"
-        "xn--r28haa.knedlsepp.at"
+        "xn--c6haa.${domain-name}"
+        "xn--yr8haa.${domain-name}"
+        "xn--0r8haa.${domain-name}"
+        "xn--1r8haa.${domain-name}"
+        "xn--2r8haa.${domain-name}"
+        "xn--3r8haa.${domain-name}"
+        "xn--4r8haa.${domain-name}"
+        "xn--5r8haa.${domain-name}"
+        "xn--6r8haa.${domain-name}"
+        "xn--7r8haa.${domain-name}"
+        "xn--8r8haa.${domain-name}"
+        "xn--9r8haa.${domain-name}"
+        "xn--g6haa.${domain-name}"
+        "xn--r28haa.${domain-name}"
       ];
       enableACME = true;
       forceSSL = true;
@@ -94,7 +96,7 @@
             <html lang="de">
             <head>
               <meta charset="utf-8">
-              <title>❤️❤️❤️.knedlsepp.at</title>
+              <title>❤️❤️❤️.${domain-name}</title>
               <style>
               h1 {
                   display: block;
@@ -110,17 +112,17 @@
         }; in
       "${site}/share/www/";
     };
-    virtualHosts."gogs.knedlsepp.at" = {
+    virtualHosts."gogs.${domain-name}" = {
       enableACME = true;
       forceSSL = true;
       locations."/".proxyPass = "http://127.0.0.1:3000";
     };
-    virtualHosts."hydra.knedlsepp.at" = {
+    virtualHosts."hydra.${domain-name}" = {
       enableACME = true;
       forceSSL = true;
       locations."/".proxyPass = "http://127.0.0.1:3001";
     };
-    virtualHosts."uwsgi-example.knedlsepp.at" = {
+    virtualHosts."uwsgi-example.${domain-name}" = {
       enableACME = true;
       forceSSL = true;
       locations."/" = {
@@ -130,12 +132,12 @@
         '';
       };
     };
-    virtualHosts."shell.knedlsepp.at" = {
+    virtualHosts."shell.${domain-name}" = {
       enableACME = true;
       forceSSL = true;
       locations."/".proxyPass = "http://127.0.0.1:4200";
     };
-    virtualHosts."mattermost.knedlsepp.at" = {
+    virtualHosts."mattermost.${domain-name}" = {
       enableACME = true;
       forceSSL = true;
       locations."/" = {
@@ -170,7 +172,7 @@
 
   services.mattermost = {
     enable = true;
-    siteUrl = "https://mattermost.knedlsepp.at";
+    siteUrl = "https://mattermost.${domain-name}";
     extraConfig = {
       EmailSettings = {
         SendEmailNotifications = true; # TODO: Set up SMTP server
@@ -182,7 +184,7 @@
   services.gogs = {
     appName = "Knedlgit";
     enable = true;
-    rootUrl = "https://gogs.knedlsepp.at/";
+    rootUrl = "https://gogs.${domain-name}/";
     extraConfig = ''
       [service]
       DISABLE_REGISTRATION = true
@@ -194,8 +196,8 @@
 
   services.hydra = {
     enable = true;
-    hydraURL = "https://hydra.knedlsepp.at";
-    notificationSender = "hydra@knedlsepp.at";
+    hydraURL = "https://hydra.${domain-name}";
+    notificationSender = "hydra@${domain-name}";
     port = 3001;
     minimumDiskFree = 1; #GiB
     useSubstitutes = true;
